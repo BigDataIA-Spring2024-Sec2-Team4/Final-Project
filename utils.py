570edafd04032c1ebb92acb61493bc9b877bf40d
Nidhi Kulkarni <kulkarni.nidh@northeastern.edu>
2024-04-25 17:43:59 -0400

    Integration and bug fixes

--- utils.py
+++ utils.py
@@ -3,8 +3,14 @@ import os
 from dotenv import load_dotenv
 import base64
 import streamlit as st
+from langchain_pinecone import PineconeVectorStore
+from langchain.vectorstores import Pinecone as pic
+from langchain.embeddings import OpenAIEmbeddings
+import os
+
 load_dotenv()
 api_key = os.getenv("openai_api_key")
+pinecone_key = os.getenv("PINECONE_API_KEY")
 
 client = OpenAI(api_key=api_key)
 
@@ -19,6 +25,30 @@ def speech_to_text(audio_data):
         return transcript, None        
     except Exception as e:
         return None, str(e)
+    
+def query_to_id(query, content_type):
+
+    if content_type == "tv_show":
+        namespace = "tv shows"
+    else:
+        namespace = "movies"
+
+    embeddings = OpenAIEmbeddings()
+    vector_store = PineconeVectorStore(
+        pic.get_pinecone_index("bot"),
+        embedding=embeddings,
+        namespace=namespace
+    )
+
+    results = vector_store.similarity_search(query, k=16)
+    ids = ','.join(result.metadata['unique_id'] for result in results)
+
+    # ids = ''
+    # for result in results:
+    #     ids += result.metadata['unique_id']
+    #     if result != results[-1]:
+    #         ids += ','
+    return ids    
 
 
     
